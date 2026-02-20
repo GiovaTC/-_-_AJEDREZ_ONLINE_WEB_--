9828ce4714ad0c2af48b8c00be48a6601b1e0fa3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-20 18:04:08 -0500

    commit - Program / .

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,17 @@
+using OnlineChess.Server.Data;
+using System.Diagnostics;
+using Oracle.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Oracle 19c
+builder.Services.AddDbContext<ChessDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleChessDB")));
+builder.Services.AddSignalR();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
