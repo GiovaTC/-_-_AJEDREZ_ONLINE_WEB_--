d93b2c7ab1e8ccc7cdf80d5b22c8bada8c66637c
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-20 17:52:50 -0500

    commit - Move / .

--- Server/Models/Move.cs
+++ Server/Models/Move.cs
@@ -2,5 +2,14 @@
 {
     public class Move
     {
-    }
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int FromX { get; set; }
+
+        public int FromY { get; set; }
+
+        public string ToX { get; set; }
+
+        public string ToY { get; set; }
+    }   
 }
