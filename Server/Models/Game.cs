1e792cc0821f1da59a0c1adb866bb5d245f3cda2
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-20 17:50:40 -0500

    commit - Game

--- Server/Models/Game.cs
+++ Server/Models/Game.cs
@@ -1,6 +1,14 @@
-﻿namespace OnlineChess.Server.Models
+﻿using System.Collections.Generic;
+
+namespace OnlineChess.Server.Models
 {
     public class Game
     {
+        public int Id { get; set; }
+        public int WhitePlayerId { get; set; }
+        public int BlackPlayerId { get; set; }
+        public bool WhiteTurn { get; set; } = true;
+
+        public List<Move> Moves { get; set; }
     }
-}
+}   
