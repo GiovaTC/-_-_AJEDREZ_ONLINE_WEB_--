da861958164b835499a1b85538358819b5344b17
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2026-02-22 10:36:36 -0500

    Add files via upload

--- OnlineChess/Program.cs
+++ OnlineChess/Program.cs
@@ -1,4 +1,28 @@
-﻿namespace OnlineChess.Server
+﻿using OnlineChess.Server.Data;
+using OnlineChess.Server.Hubs;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ChessDbContext>(options =>
+    options.UseOracle
+    (builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.MapHub<ChessHub>("/chesshub");
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
+namespace OnlineChess.Server
 {
     public class Program
     {
